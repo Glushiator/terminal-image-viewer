d04fb880fe1fcd7e7bfa96d54de9179f69a4242c
Glushiator <Glushiator@users.noreply.github.com>
2020-09-05 13:27:57 -0400

    Update img-viewer.py
    automatic image scaling to fit terminal's width also capping the width to prevent line overflow

--- img-viewer.py
+++ img-viewer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import argparse
+import os
+
 import PIL.Image
 
 
@@ -18,10 +20,10 @@ def get_color(r, g, b):
     return "\x1b[48;5;{}m \x1b[0m".format(int(get_ansi_color_code(r, g, b)))
 
 
-def show_image(img_path, rows, aspect):
+def show_image(img_path, rows, max_columns, aspect):
     img = PIL.Image.open(img_path)
 
-    columns = int((aspect * rows * img.width) / img.height)
+    columns = min(max_columns, int((aspect * rows * img.width) / img.height))
     img: PIL.Image.Image = img.resize((columns, rows), PIL.Image.ANTIALIAS)
 
     for row in range(rows):
@@ -34,10 +36,15 @@ def show_image(img_path, rows, aspect):
 def _main():
     _arg_parser = argparse.ArgumentParser(description="console image view utility")
     _arg_parser.add_argument('image', help="image file")
-    _arg_parser.add_argument('--rows', '-R', help='rows to use', type=int, default=25)
+    _arg_parser.add_argument('--rows', '-R', help='rows to use', type=int)
     _arg_parser.add_argument('--aspect', '-A', help='console "pixel" aspect h/w', type=float, default=2.8)
     _args = _arg_parser.parse_args()
-    show_image(_args.image, _args.rows, _args.aspect)
+    _columns, _rows = os.get_terminal_size()
+    if not _args.rows:
+        _rows = int(_columns / _args.aspect)
+    else:
+        _rows = _args.rows
+    show_image(_args.image, _rows, _columns, _args.aspect)
 
 
 if __name__ == '__main__':
